4b5def76392fc3da380d99ca429d0faae8f538ff
csdojo <sherrywanwang@gmail.com>
2019-02-12 19:35:55 -0500

    install ifenv again

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-// // import dependencies
-// const express = require('express');
-// const path = require('path');
-// const mongoose = require('mongoose');
-// const routes = require('./routes');
-// const PORT = process.env.PORT || 3001;
+// import dependencies
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const PORT = process.env.PORT || 3001;
 
-// // set up express server
-// const app = express();
+// set up express server
+const app = express();
 
-// // set up express middleware
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
+// set up express middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-// // serve up front end from server ONLY if in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+// serve up front end from server ONLY if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// // turn on routes
-// app.use(routes);
+// turn on routes
+app.use(routes);
 
-// // set up a wildcard route just in case all of the other routes fail
-// app.get("*", function(req,res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+// set up a wildcard route just in case all of the other routes fail
+app.get("*", function(req,res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-// // turn on mongo connection
-// // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
+// turn on mongo connection
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
-// // turn on server
-// app.listen(PORT, () => console.log(`🗺️ ==> Server now on ${PORT}`))
+// turn on server
+app.listen(PORT, () => console.log(`🗺️ ==> Server now on ${PORT}`))
